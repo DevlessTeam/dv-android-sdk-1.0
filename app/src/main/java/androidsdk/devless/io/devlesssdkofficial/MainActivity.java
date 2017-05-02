7ce36a9e4e876e147e9b9ad27ba88e6da24eeb99
charles agyemang <micnkru@gmail.com>
2017-05-02 11:52:39 +0000

    creayted the arr file in the directory

--- app/src/main/java/androidsdk/devless/io/devlesssdkofficial/MainActivity.java
+++ app/src/main/java/androidsdk/devless/io/devlesssdkofficial/MainActivity.java
@@ -159,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
             public void OnLogInFailed(String error) {
                 Log.e("UsernamePassLoginSucc", error);
             }
-        });
+        })
         */
 
 
